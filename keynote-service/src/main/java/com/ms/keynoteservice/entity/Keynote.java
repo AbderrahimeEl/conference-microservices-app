a6df9e4c2f5123629bf3667879b4ac9160ca2c72
AbderrahimeEl <elmoutaouakil12345@gmail.com>
2025-10-20 10:00:51 +0100

    implement keynoteservice using RepositoryRestResource

---
+++ keynote-service/src/main/java/com/ms/keynoteservice/entity/Keynote.java
@@ -0,0 +1,23 @@
+package com.ms.keynoteservice.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Keynote {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String LastName;
+    private String email;
+    private String function;
+}
