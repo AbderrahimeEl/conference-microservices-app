b13f19683cd5f84de1fa811d403b30798784fa22
AbderrahimeEl <elmoutaouakil12345@gmail.com>
2025-10-21 18:45:35 +0100

    Add CommandLineRunner beans to populate Conference and Keynote data on startup

--- keynote-service/src/main/java/com/ms/keynoteservice/KeynoteServiceApplication.java
+++ keynote-service/src/main/java/com/ms/keynoteservice/KeynoteServiceApplication.java
@@ -1,13 +1,27 @@
 package com.ms.keynoteservice;
 
+import com.ms.keynoteservice.entity.Keynote;
+import com.ms.keynoteservice.repository.KeynoteRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KeynoteServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KeynoteServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner c(KeynoteRepository keynoteRepository) {
+        return args -> {
+            for (int i = 0; i < 10; i++) {
+                Keynote keynote = new Keynote(null,"keynote "+i,"idk","keynote"+i+"@gmail.com","func"+i);
+                keynoteRepository.save(keynote);
+            }
+        };
     }
 
 }
